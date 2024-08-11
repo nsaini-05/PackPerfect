5e196aed9af2e28c8e8658aa4d1ec417cffa07b8
Neeraj Saini <asaini7788@gmail.com>
2024-08-11 00:19:26 -0600

    Initial Commit

--- src/Components/InputForm.jsx
+++ src/Components/InputForm.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
-
+import { Dropdown } from "primereact/dropdown";
 export const InputForm = () => {
+  const [itemName, setItemName] = useState("");
+  const [itemQuantity, setItemQuantity] = useState(1);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setItemName("");
+    setItemQuantity(1);
+  };
   return (
-    <div className="flex 	justify-content-center	gap-2 mb-8">
-      <InputText type="text" className="p-inputtext-lg" placeholder="Large" />
-      <Button label="Submit" />
+    <div>
+      <form
+        className="flex 	justify-content-center	gap-2 mb-8"
+        onSubmit={(e) => handleFormSubmit(e)}
+      >
+        <InputText
+          type="text"
+          className="p-inputtext-lg"
+          placeholder="Item Name"
+          value={itemName}
+          onChange={(e) => setItemName(e.target.value)}
+        />
+        <Dropdown
+          value={itemQuantity}
+          options={Array.from({ length: 20 }, (_, i) => 1 + i)}
+          placeholder="Select a Quantity"
+          className="p-inputtext-lg"
+          onChange={(e) => Number(setItemQuantity(e.target.value))}
+        />
+        <Button label="Submit" disabled={!itemName} />
+      </form>
     </div>
   );
 };
--- src/Components/ItemsList.jsx
+++ src/Components/ItemsList.jsx
@@ -4,15 +4,8 @@ import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
 ];
+
 export const ItemsList = () => {
   const [itemsToBePacked, setItemsToBePacked] = useState(initialItems);
   const [itemsAlreadyPacked, setItemsAlreadyPacked] = useState([]);
